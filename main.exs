433c1441f4226ae93969e59324c234048d854700
AlieksandrVoish <26520580-AlieksandrVoish@users.noreply.replit.com>
2023-11-15 16:48:22 +0000

    Initial commit

---
+++ main.exs
@@ -0,0 +1,19 @@
+defmodule Lesson do
+  def start do
+    IO.puts(div(5,2))
+    IO.puts(rem(5,2))
+  end
+  
+ 
+  def math1(n1,n2) do
+   #(1+2)/2 
+   #
+   IO.puts((n1+n2)/n2)
+   IO.puts(div(n1,n2))
+   IO.puts(rem(n2,n1))
+   
+   
+  end
+
+  
+end
